b54ea3a9b97e008d2db811ff4bf2f8a1120ce1a9
AnderGonzalezA <angoaminf3@gmail.com>
2021-02-02 19:49:18 +0100

    actualizacion de librerias

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
-import net from 'net'
+import net from "net";
 
-const socket = net.Socket()
-socket.connect(8000, 'localhost')
+const socket = net.Socket();
+socket.connect(8000, "localhost");
 
-socket.write('Hola?')
+socket.write("Hola?");
 
-socket.on('data', data => console.log(d.toString()))
+socket.on("data", (data) => console.log(d.toString()));
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import net from 'net'
+import net from "net";
 
-const server = net.createServer(socket => {
-  socket.on('data', data => {
-    console.log(data.toString())
-    socket.write('Mundo?')
-  })
-})
+const server = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    console.log(data.toString());
+    socket.write("Mundo?");
+  });
+});
 
-server.on('error', err => {
-  throw err
-})
+server.on("error", (err) => {
+  throw err;
+});
 
-server.on('connect', () => console.log('socket connected'))
+server.on("connect", () => console.log("socket connected"));
 
 server.listen(
   {
-    host: 'localhost',
+    host: "localhost",
     port: 8000,
-    exclusive: true
+    exclusive: true,
   },
-  () => console.log('Servidor socket abierto en ', server.address())
-)
+  () => console.log("Servidor socket abierto en ", server.address())
+);
