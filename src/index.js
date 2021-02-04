408650078a04442519e42bdf6c139c974488b01d
AnderGonzalezA <angoaminf3@gmail.com>
2021-02-04 20:07:39 +0100

    actualizacion de librerias y dependencias

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import http from 'http'
+import http from "http";
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' })
-  // response.setHeader('Content-Type', 'application/json')
-  // response.statusCode = 404
+  // response.writeHead(200, { "Content-Type": "text/html", charset: "UTF-8" });
+  // response.writeHead(400, { "Content-Type": "application/json", charset: "UTF-8" });
+  response.setHeader("Content-Type", "application/json");
+  response.statusCode = 301;
+  //response.statusCode = 404;
 
-  if (request.method === 'GET') {
-    response.write('<h1>Metodo valido</h1>')
-    return response.end()
+  if (request.method === "GET") {
+    response.write("<h1>Metodo valido</h1>");
+    return response.end();
   }
 
-  response.write('<h1>Esta intentando acceder con un metodo no valido</h1>')
-  return response.end()
-})
+  response.write("<h1>Esta intentando acceder con un metodo no valido</h1>");
+  return response.end();
+});
 
-server.listen(8000, 'localhost', err => {
+server.listen(8000, "localhost", (err) => {
   if (err) {
-    return console.log('Error: ', err)
+    return console.log("Error: ", err);
   }
 
-  console.log('Server opened listen on http://localhost:8000')
-})
+  console.log("Server opened listen on http://localhost:8000");
+});
